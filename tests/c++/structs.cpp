47e32e41dae4cd56a20a6e0abaa7c259c3f09ea7
twyst.development <development@twyst.co.za>
2022-10-28 14:16:17 -0700

    Added manual run to github workflow

--- tests/c++/structs.cpp
+++ tests/c++/structs.cpp
@@ -9,8 +9,8 @@ using namespace std;
 bool structs(uhppoted &u) {
     vector<controller> controllers = {};
 
-    uhppoted utrue("0.0.0.0", "255.255.255.255", "0.0.0.0:60001", 2500, controllers, true);
-    uhppoted ufalse("0.0.0.0", "255.255.255.255", "0.0.0.0:60001", 2500, controllers, false);
+    uhppoted utrue("0.0.0.0", "255.255.255.255:60000", "0.0.0.0:60001", 2500, controllers, true);
+    uhppoted ufalse("0.0.0.0", "255.255.255.255:60000", "0.0.0.0:60001", 2500, controllers, false);
 
     auto d1 = utrue.get_device(0xffffffff);
     auto d2 = ufalse.get_device(0xfffffffe);
